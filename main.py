b3b69ac0f3db5d3b1ae4e55cd1d14f9673fe04fb
Sykerike <98282970+Sykerike@users.noreply.github.com>
2022-12-15 13:08:33 +0100

    primer commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800,height=600)
+screen.title("pong")
+
+screen.tracer(0)
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+
+
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up,"Up")
+screen.onkey(r_paddle.go_down,"Down")
+
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down,"s")
+
+game_is_on = True
+
+
+
+
+while True:
+
+    time.sleep(0.01)
+    screen.update()
+    ball.move()
+
+    #Detect collision.
+    if ball.ycor() > 288 or ball.ycor() < -288:
+        #Needs to bounce
+        ball.bounce_y()
+
+    #Detect collision paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < - 340:
+        ball.bounce_x()
+
+    #Detect ball pass beyond r_paddle
+    if ball.xcor() > 405:
+        ball.reset_position()
+        scoreboard.l_point()
+
+
+    if ball.xcor() <  -405:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
+
